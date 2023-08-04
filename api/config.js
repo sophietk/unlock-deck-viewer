3c00fd03cafb01d51c07abf21e96c83fc6a90b5b
sophietk <sophie.tk@gmail.com>
2023-08-04 16:27:25 +0200

    refactor: rework nbCards

--- api/config.js
+++ api/config.js
@@ -8,11 +8,11 @@ module.exports.decks = [
     id: '59baa2_5dbfd556434846bf8754ca4116101f37',
     name: 'Tutoriel',
     difficulty: '☆☆☆',
+    nbCards: 12,
     pdfCardWidth: 721 * ratioDensity,
     pdfCardHeight: 1323 * ratioDensity,
     pdfMarginLeft: 146 * ratioDensity,
     pdfMarginTop: 276 * ratioDensity,
-    nbCards: 12,
     colPerPage: 3,
     getPageNumber: (cardId, face) => {
       const pageNumber = Math.floor(cardId / 6) * 2 + 1
@@ -26,11 +26,11 @@ module.exports.decks = [
     id: '59baa2_b414561726024995b66b03ecef774c8b',
     name: 'La 5e avenue',
     difficulty: '★☆☆',
+    nbCards: 27,
     pdfCardWidth: 721 * ratioDensity,
     pdfCardHeight: 1323 * ratioDensity,
     pdfMarginLeft: 146 * ratioDensity,
     pdfMarginTop: 276 * ratioDensity,
-    nbCards: 27,
     colPerPage: 3,
     getPageNumber: (cardId, face) => {
       const pageNumber = Math.floor(cardId / 6) * 2 + 1
@@ -44,11 +44,11 @@ module.exports.decks = [
     id: '59baa2_f33934a78d5b438e8cd3dafc2ae1b07f',
     name: 'Le foie de l’Axolotl',
     difficulty: '★☆☆',
+    nbCards: 24,
     pdfCardWidth: 721 * ratioDensity,
     pdfCardHeight: 1323 * ratioDensity,
     pdfMarginLeft: 160 * ratioDensity,
     pdfMarginTop: 426 * ratioDensity,
-    nbCards: 24,
     colPerPage: 3,
     getPageNumber: (cardId, face) => {
       const pdfPagesToSkip = 2
@@ -63,11 +63,11 @@ module.exports.decks = [
     id: '59baa2_91481bc69eda4c75b6ef318135d8508b',
     name: 'Cambriolage fantôme',
     difficulty: '★☆☆',
+    nbCards: 24,
     pdfCardWidth: 778 * ratioDensity,
     pdfCardHeight: 1321 * ratioDensity,
     pdfMarginLeft: 462 * ratioDensity,
     pdfMarginTop: 252 * ratioDensity,
-    nbCards: 24,
     colPerPage: 2,
     cardsPerPage: 4,
     getPageNumber: (cardId, face) => {
@@ -85,11 +85,11 @@ module.exports.decks = [
     id: '59baa2_fcba9236512e4b2b912e26150ca01264',
     name: 'L’élite',
     difficulty: '★★☆',
+    nbCards: 21,
     pdfCardWidth: 721 * ratioDensity,
     pdfCardHeight: 1323 * ratioDensity,
     pdfMarginLeft: 146 * ratioDensity,
     pdfMarginTop: 276 * ratioDensity,
-    nbCards: 21,
     colPerPage: 3,
     getPageNumber: (cardId, face) => {
       const pageNumber = Math.floor(cardId / 6) * 2 + 1
@@ -103,11 +103,11 @@ module.exports.decks = [
     id: '59baa2_ec1d34bb0fe246069dd7dc12ebf8697d',
     name: 'À la poursuite de Cabrakan',
     difficulty: '★★☆',
+    nbCards: 24,
     pdfCardWidth: 721 * ratioDensity,
     pdfCardHeight: 1323 * ratioDensity,
     pdfMarginLeft: 128 * ratioDensity,
     pdfMarginTop: 505 * ratioDensity,
-    nbCards: 24,
     colPerPage: 3,
     getPageNumber: (cardId, face) => {
       let pageNumber = Math.floor(cardId / 6) * 2
@@ -126,11 +126,11 @@ module.exports.decks = [
     id: '59baa2_d7f24d940eea4cfa89b7400dfb581459',
     name: 'Le donjon de Doo-Arann',
     difficulty: '★★☆',
+    nbCards: 24,
     pdfCardWidth: 721 * ratioDensity,
     pdfCardHeight: 1323 * ratioDensity,
     pdfMarginLeft: 128 * ratioDensity,
     pdfMarginTop: 505 * ratioDensity,
-    nbCards: 24,
     colPerPage: 3,
     getPageNumber: (cardId, face) => {
       let pageNumber = Math.floor(cardId / 6) * 2
@@ -149,11 +149,11 @@ module.exports.decks = [
     id: 'Noel_en_juillet-',
     name: 'Noël en juillet',
     difficulty: '★★☆',
+    nbCards: 27,
     pdfCardWidth: 721 * ratioDensity,
     pdfCardHeight: 1323 * ratioDensity,
     pdfMarginLeft: 160 * ratioDensity,
     pdfMarginTop: 430 * ratioDensity,
-    nbCards: 27,
     colPerPage: 3,
     getPageNumber: (cardId, face) => {
       const pageNumber = Math.floor(cardId / 6) * 2 + 1
@@ -167,11 +167,11 @@ module.exports.decks = [
     id: '59baa2_164fbfd3e76b4dbcb46282074693f098',
     name: 'Le temple de RA',
     difficulty: '★★★',
+    nbCards: 24,
     pdfCardWidth: 721 * ratioDensity,
     pdfCardHeight: 1323 * ratioDensity,
     pdfMarginLeft: 146 * ratioDensity,
     pdfMarginTop: 276 * ratioDensity,
-    nbCards: 24,
     colPerPage: 3,
     getPageNumber: (cardId, face) => {
       const pageNumber = Math.floor(cardId / 6) * 2 + 1
